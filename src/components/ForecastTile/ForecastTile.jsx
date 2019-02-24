00b960c72a26059f6f45dda0d273f234745d4d3a
Henry Jordan <henry.jordan35@gmail.com>
2019-02-24 11:04:14 -0600

    ADDED | Dynamic logic to determine status icon for each tile

--- src/components/ForecastTile/ForecastTile.jsx
+++ src/components/ForecastTile/ForecastTile.jsx
@@ -1,14 +1,48 @@
 import React, { PureComponent } from 'react';
+import moment from 'moment';
 
+// Assets
+import drizzleSvg from '../../assets/drizzle.svg';
+import partCloudySvg from '../../assets/part-cloudy.svg';
+import rainSvg from '../../assets/rain.svg';
+import thunderSvg from '../../assets/rain.svg';
+
+import * as codes from '../../utilities/ComponentConstants';
 import styles from './ForecastTile.module.css';
 
 class ForecastTile extends PureComponent {
+    setWeatherState() {
+        let { weatherData } = this.props;
+        let icon;
+        switch (true) {
+            case (weatherData.id >= codes.ATMOSPHERE_CODE):
+                icon = <img src={partCloudySvg} className={styles['type-img']} alt="Card cap" />;
+                break;
+            case (weatherData.id >= codes.RAIN_CODE):
+                icon = <img src={rainSvg} className={styles['type-img']} alt="Card cap" />;
+                break;
+            case (weatherData.id >= codes.DRIZZLE_CODE):
+                icon = <img src={drizzleSvg} className={styles['type-img']} alt="Card cap" />;
+                break;
+            case (weatherData.id >= codes.THUNDER_CODE):
+                icon = <img src={thunderSvg} className={styles['type-img']} alt="Card cap" />;
+                break;
+            default:
+                break;
+        }
+        return icon;
+    }
+
     render() {
+        let { weatherData, weatherDate } = this.props;
+        let weatherStatus = this.setWeatherState();
         return (
             <React.Fragment>
                 {this.props.weatherData && (
                     <div className={styles['temp-tile']}>
-                        <p>{this.props.weatherData.tempFahrenheit}</p>
+                        <p>{moment.unix(weatherDate).format('ddd')}</p>
+                        {weatherStatus}
+                        <p>{weatherData.tempFahrenheit}</p>
                     </div>)
                 }
             </React.Fragment>
---
+++ src/utilities/ComponentConstants.jsx
@@ -0,0 +1,7 @@
+export const THUNDER_CODE = 200;
+export const DRIZZLE_CODE = 300;
+export const RAIN_CODE=500;
+export const SNOW_CODE=600;
+export const ATMOSPHERE_CODE=700;
+export const CLEAR_CODE=800;
+export const CLOUD_CODE=801;
